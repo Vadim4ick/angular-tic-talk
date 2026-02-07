f42410e675e8d15db318d8e6ef33fe2a8cc71d97
Vadim <a-yva-2013@mail.ru>
2026-02-07 15:31:24 +0400

    ✨ (avatar-upload): добавлена поддержка перетаскивания файлов для загрузки аватара 📝 (dnd.ts): создана директива для обработки событий перетаскивания Изменения добавляют функциональность перетаскивания файлов для загрузки аватара, что улучшает пользовательский интерфейс и делает процесс загрузки более интуитивным. Создана новая директива Dnd для обработки событий перетаскивания, что позволяет выделять область загрузки и обрабатывать файлы, перетаскиваемые пользователем.

--- src/pages/settings/avatar-upload/avatar-upload.ts
+++ src/pages/settings/avatar-upload/avatar-upload.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { IconsModule } from '@/shared/lucide/lucide-module';
+import { Dnd } from '@/shared/helpers/dirictive/dnd';
 
 @Component({
   selector: 'app-avatar-upload',
-  imports: [IconsModule],
+  imports: [IconsModule, Dnd],
   templateUrl: './avatar-upload.html',
   styleUrl: './avatar-upload.scss',
 })
@@ -14,6 +15,14 @@ export class AvatarUpload {
     const target = event.target as HTMLInputElement;
     const file = target?.files?.[0];
 
+    this.proccessFile(file);
+  }
+
+  onFileDropped(file: File) {
+    this.proccessFile(file);
+  }
+
+  proccessFile(file: File | undefined) {
     if (!file || !file.type.startsWith('image/')) return;
 
     const reader = new FileReader();
